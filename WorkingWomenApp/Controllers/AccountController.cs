903f21133bee18e1e6dc3ec418b1a84a15e5cad9
monicaiyb <monicaiyb@gmail.com>
2025-07-23 01:43:00 +0300

    Add login page and controller

---
+++ WorkingWomenApp/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkingWomenApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
