29eb203b766909b87eda4cae21631d6a8bf7c52b
monicaiyb <monicaiyb@gmail.com>
2025-07-26 01:08:48 +0300

    Add weather api service

---
+++ WorkingWomenApp.BLL/Implementation/WeatherApiService.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using WorkingWomenApp.BLL.Interfaces;
+using WorkingWomenApp.Database.Models.WeatherApi;
+
+namespace WorkingWomenApp.BLL.Implementation
+{
+    public class WeatherApiService:IWeatherApiService
+    {
+        public string BaseUrl { get; set; }
+        readonly HttpClient client;
+        public WeatherApiService()
+        {
+
+            BaseUrl = "https://api.met.no";
+
+            client = new HttpClient
+            {
+                BaseAddress = new Uri(BaseUrl)
+            };
+
+        }
+
+        public async Task<(bool, string, entity)> GetWeatherInfo(double latitude, double longitude)
+        {
+            string errorMessage = null;
+            bool success = false;
+          
+            entity resultingMessage = new entity();
+
+            try
+            {
+
+                var request = new HttpRequestMessage(HttpMethod.Get, $"weatherapi/locationforecast/2.0/complete?{latitude}&{(longitude)}");
+                //request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                var response = await client.SendAsync(request);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception($"An error has occurred while verifying business name");
+
+                }
+              
+                var xmlString = await response.Content.ReadAsStringAsync();
+                
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                resultingMessage = JsonConvert.DeserializeObject<entity>(apiResponse);
+
+                success = true;
+
+                //return (success, errorMessage, resultingMessage);
+            }
+            catch (Exception e)
+            {
+                //errorMessage = e.ExtractInnerExceptionMessage();
+            }
+
+            return (success, errorMessage, resultingMessage);
+        }
+    }
+}
---
+++ WorkingWomenApp.BLL/Interfaces/IWeatherApiService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkingWomenApp.Database.Core;
+using WorkingWomenApp.Database.Models.WeatherApi;
+
+namespace WorkingWomenApp.BLL.Interfaces
+{
+    public interface IWeatherApiService
+    {
+        Task<(bool, string, entity)> GetWeatherInfo(double latitude, double longitude);
+    }
+
+   
+}
+
+
