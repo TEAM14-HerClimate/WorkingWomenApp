3ff857ab0fc1590c65e95741d0cd9efa491fbbcb
monicaiyb <monicaiyb@gmail.com>
2025-10-29 16:11:07 +0300

    edit profile details page

--- WorkingWomenApp.Database/DTOs/ProfileDtos/ProfileDtos.cs
+++ WorkingWomenApp.Database/DTOs/ProfileDtos/ProfileDtos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,15 @@ namespace WorkingWomenApp.Database.DTOs.ProfileDtos
     {
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
+        [Display(Name = "Are you pregnant?")]
         public bool IsPregnant { get; set; }
+        [Display(Name = "Are you a mother?")]
         public bool IsMother { get; set; }
+        [Display(Name = "What do you do professionally/ do for a living?")]
         public Profession Profession { get; set; }
+        [Display(Name = "Describe what you professionally/ do for a living")]
         public string ProfessionDescription { get; set; }
+        [Display(Name = "Are you breastfeeding?")]
         public bool IsBreastfeeding { get; set; }
         public int NumberOfChidren { get; set; }
         public int PregnancyWeeks { get; set; }
