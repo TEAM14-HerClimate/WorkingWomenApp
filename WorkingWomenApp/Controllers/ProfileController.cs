aead817186634b65234531b202799c17e15db0c4
monicaiyb <monicaiyb@gmail.com>
2025-10-30 00:12:38 +0300

    Fix controllers to save userprofile

--- WorkingWomenApp/Controllers/ProfileController.cs
+++ WorkingWomenApp/Controllers/ProfileController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using WorkingWomenApp.Attribute;
+using WorkingWomenApp.BLL.Interfaces;
 using WorkingWomenApp.BLL.UnitOfWork;
 using WorkingWomenApp.Database.DTOs.ProfileDtos;
 using WorkingWomenApp.Database.enums;
@@ -13,11 +15,13 @@ namespace WorkingWomenApp.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
+        private readonly ISessionService _sessionService;
 
-        public ProfileController(IUnitOfWork unitOfWork, IMapper mapper)
+        public ProfileController(IUnitOfWork unitOfWork, IMapper mapper, ISessionService sessionService)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
+            _sessionService = sessionService;
         }
 
         [ProtectAction(SecurityModule.Profile, SecuritySubModule.UserProfile, SecuritySystemAction.ViewItem)]
@@ -28,23 +32,23 @@ namespace WorkingWomenApp.Controllers
         }
 
         [ProtectAction(SecurityModule.Profile, SecuritySubModule.UserProfile, SecuritySystemAction.ViewItem)]
-        public async Task<ActionResult> Details(Guid? id)
+        public async Task<ActionResult> Details(Guid? userId)
         {
-
-            var profile = await _unitOfWork.ProfileRepository.GetAsync(x => x.Id == id, includeProperties: "User"); 
-
+        
+            var profile = await _unitOfWork.ProfileRepository.GetAsync(x => x.Id == userId, includeProperties: "User");
+            var profileDtos = _mapper.Map<UserProfileDtos>(profile);
             // strongly typed view - by putting object into the view vs. ViewBag.ComicBook = comicBook;
-            return View();  // will automatically look in the views folder
+            return View(profileDtos);  // will automatically look in the views folder
         }
 
         [HttpPut]
         [HttpPost]
         [ProtectAction(SecurityModule.Profile, SecuritySubModule.UserProfile, SecuritySystemAction.CreateAndEdit)]
-        public async Task<ActionResult> Details(Guid? id, UserProfileDtos? profileDtos = null)
+        public async Task<ActionResult> Details( UserProfileDtos? profileDtos = null)
         {
 
             var profile = _mapper.Map<UserProfile>(profileDtos);
-            if (id == Guid.Empty)
+            if (profileDtos.Id == Guid.Empty)
             {
                 await _unitOfWork.ProfileRepository.AddAsync(profile);
                 
@@ -54,15 +58,25 @@ namespace WorkingWomenApp.Controllers
                 await _unitOfWork.ProfileRepository.UpdateAsync(profile);
             }
             await _unitOfWork.SaveChangesAsync();
-            return Redirect("/Index");
+            return Redirect("/Profile");
         }
-        public async Task<ActionResult> UserProfile(Guid id)
+        public async Task<ActionResult> UserProfile()
         {
-                var profile=await _unitOfWork.ProfileRepository.GetAsync(x => x.Id == id, includeProperties: "User"); 
-                  
-                var profiledto = _mapper.Map<UserProfileDtos>(profile);
-          
+            var id = _sessionService.GetUser().Id;
+            var profile=await _unitOfWork.ProfileRepository.GetAsync(x => x.UserId == id, includeProperties: "User");
+            var profiledto = _mapper.Map<UserProfileDtos>(profile);
+
+            if (profiledto == null)
+            {
+                profiledto = new UserProfileDtos
+                { 
+                    // Assign user ID as profile ID
+                    UserId = id
+                };
+
+            }
             return View(profiledto);
+           
         }
 
     }
